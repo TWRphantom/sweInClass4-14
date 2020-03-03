894c5a730399bf32f27d98edfa42c32c05cb0fbc
MMN165 <55259986+MMN165@users.noreply.github.com>
2020-03-03 18:35:27 -0500

    Update twoButtons.java

--- src/main/java/servlet/twoButtons.java
+++ src/main/java/servlet/twoButtons.java
@@ -12,6 +12,23 @@ import java.util.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
+package servlet;
+
+import java.io.PrintWriter;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(
+        name = "MyServlet",
+        urlPatterns = {"/twoButtons"}
+    )
+
 // twoButtons class
 // CONSTRUCTOR: no constructor specified (default)
 //
