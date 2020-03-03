21519fc84a45ba39da2f6c12676331a4b9ae8ddb
MMN165 <55259986+MMN165@users.noreply.github.com>
2020-03-03 18:42:33 -0500

    Update twoButtons.java

--- src/main/java/servlet/twoButtons.java
+++ src/main/java/servlet/twoButtons.java
@@ -3,7 +3,6 @@
 
         @author Jeff Offutt
 ********************************************************************* */
-package servlet;
 
 // Import Java Libraries
 import java.io.*;
@@ -18,7 +17,12 @@ package servlet;
 import java.io.PrintWriter;
 import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(
         name = "MyServlet",
